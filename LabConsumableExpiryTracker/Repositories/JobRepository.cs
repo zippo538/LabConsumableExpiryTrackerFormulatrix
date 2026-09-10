2e43774ddbe8d59209487bba3609b6b4c8e70c8a
Mahindra Irvan Saputra <uchihas538.zippo@gmail.com>
2026-09-10 14:20:37 +0700

    feat : add job repository

---
+++ LabConsumableExpiryTracker/Repositories/JobRepository.cs
@@ -0,0 +1,25 @@
+using LabConsumableExpiryTracker.Data.Seeders;
+using LabConsumableExpiryTracker.Models;
+using LabConsumableExpiryTracker.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabConsumableExpiryTracker.Repositories
+{
+    public class JobRepository : Repository<Job, Guid>, IJobRepository
+    {
+        public JobRepository(AppDbContext context) : base(context)
+        {
+            
+        }
+
+
+        public Task<Job?> GetByJobNumberAsync(string jobNumber, CancellationToken ct = default)
+        {
+            if (string.IsNullOrWhiteSpace(jobNumber)) throw new ArgumentException("Job number is required.", nameof(jobNumber));
+            var normalizedJobNumber = jobNumber.Trim();
+            return DbSet.SingleOrDefaultAsync(job => job.JobNumber == normalizedJobNumber, ct);
+        }
+
+
+    }
+}
